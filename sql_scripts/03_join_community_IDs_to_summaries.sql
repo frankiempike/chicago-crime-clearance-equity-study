69ebb8119966eebe56ba68354f90207142079265
Frankie Pike <155915453+frankiempike@users.noreply.github.com>
2026-04-14 18:10:11 -0500

    Fix ambiguous joins and column references

--- sql_scripts/03_join_community_IDs_to_summaries.sql
+++ sql_scripts/03_join_community_IDs_to_summaries.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE TABLE `chicago_justice.crime_analysis_by_community` AS
-SELECT 
+SELECT
     c.*,
-    s.* EXCEPT (Community_Area)
+    s.* EXCEPT (community_area)
 FROM `chicago_justice.crime_data` AS c
-JOIN `chicago_justice.community_IDs` AS ids
+INNER JOIN `chicago_justice.community_IDs` AS ids
     ON c.community_area = ids.area_numbe
-JOIN `chicago_justice.community_summaries` AS s
-    ON ids.COMMUNITY = s.Community_Area
+INNER JOIN `chicago_justice.community_summaries` AS s
+    ON ids.community = s.community_area
 ORDER BY c.date, c.community_area;
