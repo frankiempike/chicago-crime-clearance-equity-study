0203c2f36b8cdedeb6ad064e1b1eee7d53a94f0e
Frankie Pike <155915453+frankiempike@users.noreply.github.com>
2026-04-14 15:11:06 -0500

    Fix ambiguous joins and column references

--- sql_scripts/03_join_community_IDs_to_summaries.sql
+++ sql_scripts/03_join_community_IDs_to_summaries.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE `chicago_justice.crime_analysis_by_community` AS
 SELECT 
     c.*,
-    s.* EXCEPT(Community_Area) 
+    s.* EXCEPT (Community_Area)
 FROM `chicago_justice.crime_data` AS c
 JOIN `chicago_justice.community_IDs` AS ids
     ON c.community_area = ids.area_numbe
--- sql_scripts/04_analyze_unsolved_rate_by_crime_type.sql
+++ sql_scripts/04_analyze_unsolved_rate_by_crime_type.sql
@@ -4,6 +4,6 @@ SELECT
     ROUND(100 - (AVG(CASE WHEN arrest THEN 1 ELSE 0 END) * 100), 2)
         AS unsolved_percentage
 FROM `chicago_justice.crime_analysis_by_community`
-GROUP BY 1
+GROUP BY primary_type
 HAVING COUNT(*) > 10
 ORDER BY unsolved_percentage DESC;
