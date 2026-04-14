232d68bdd3c86e76768ccfbf29bf7a1e5c14c30b
Frankie Pike <155915453+frankiempike@users.noreply.github.com>
2026-04-14 15:07:37 -0500

    Manual fix of SQL linting errors

--- sql_scripts/03_join_community_IDs_to_summaries.sql
+++ sql_scripts/03_join_community_IDs_to_summaries.sql
@@ -1,1 +1,10 @@
-REPLACE TABLE `chicago_justice.community_summaries` AS
+CREATE OR REPLACE TABLE `chicago_justice.crime_analysis_by_community` AS
+SELECT 
+    c.*,
+    s.* EXCEPT(Community_Area) 
+FROM `chicago_justice.crime_data` AS c
+JOIN `chicago_justice.community_IDs` AS ids
+    ON c.community_area = ids.area_numbe
+JOIN `chicago_justice.community_summaries` AS s
+    ON ids.COMMUNITY = s.Community_Area
+ORDER BY c.date, c.community_area;
